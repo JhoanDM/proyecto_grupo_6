4c174f9751d169b44796d838713d5de9b8a3eb43
Jesus Colmenares <qwejesusandres@gmail.com>
2026-02-16 23:06:18 -0500

    se cambiaron los estilos a varias paginas como la de barbero y cliente y sus ramificaciones para dejar un estilo estardar, se linkea mas paginas como el historia del cliente y ver citas del cliente como la del barbero, tambien se hizo scripts para identificar quien inicio sesion segun el rol elegido

--- js/Sesion.js
+++ js/Sesion.js
@@ -1,17 +1,58 @@
+let rolSeleccionado = null;
+
 function seleccionarRol(boton) {
-    document.querySelectorAll('.roles .BotonDefecto').forEach(btn => {btn.classList.remove('activo');
-    });
+    document.querySelectorAll('.roles .BotonDefecto')
+        .forEach(btn => btn.classList.remove('activo'));
 
-boton.classList.add('activo');
+    boton.classList.add('activo');
 
-const rol = boton.textContent.trim();
+    rolSeleccionado = boton.textContent.trim();
 
-const registro = document.querySelector('.registro');
+    const registro = document.querySelector('.registro');
 
-if (rol === 'Cliente') {
-    registro.style.display = 'block';
-} else {
-    registro.style.display = 'none';
+    if (rolSeleccionado === 'Cliente') {
+        registro.style.display = 'block';
+    } else {
+        registro.style.display = 'none';
+    }
 }
 
+function iniciarSesion() {
+
+    const correo = document.querySelector('input[type="email"]').value;
+    const password = document.querySelector('input[type="password"]').value;
+
+    if (!rolSeleccionado) {
+        alert("Selecciona un rol");
+        return;
+    }
+
+    if (!correo || !password) {
+        alert("Completa todos los campos");
+        return;
+    }
+
+    const usuario = {
+        nombre: correo.split("@")[0],
+        rol: rolSeleccionado
+    };
+
+    localStorage.setItem("usuario", JSON.stringify(usuario));
+    //segun el rol seleccionado lo mandara a una pantalla diferente, si es cliente a la pantalla de inicio, si es barbero a su perfil y si es administrador a la pantalla de citas
+
+    if (rolSeleccionado === "Cliente") {
+        window.location.href = "/html/pantallaInicio.html";
+    }
+
+    else if (rolSeleccionado === "Barbero") {
+        window.location.href = "/html/barbero/inicio_barbero_cita.html";
+    }
+
+    else if (rolSeleccionado === "Administrador") {
+        window.location.href = "/html/admin/inicio_admin_reportes.html";
+    }
+
 }
+
+
+//se elimino la funcion const rol = boton.textContent.trim(); Se eliminó porque era una variable local que no podía ser usada fuera de la funcion, se reemplazo por el let rolSeleccionado = null; esto guarda el rol seleccionado pero si no se selecciona ningun rol, este mandara una alerta, despues de selecionar e iniciar sesion, se guarda el usuario en el localStorage y se redirige a la pantalla de inicio
